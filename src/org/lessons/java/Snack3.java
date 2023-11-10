5d733296983e6d529f4e4ad98c2a649a00b0ae79
TALEX1995 <tanase.alex@libero.it>
2023-11-10 11:49:24 +0100

    snack3

---
+++ src/org/lessons/java/Snack3.java
@@ -0,0 +1,20 @@
+package org.lessons.java;
+
+public class Snack3 {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {5, 23, 54, 1, 53, 626, 34, 44, 25, 523, 30, 347};
+		
+		int sum = 0;
+		
+		for(int i = 0; i < numbers.length; i++) {
+			if(i % 2 == 1) {
+				sum += numbers[i];
+			}
+		}
+		
+		System.out.println("La somma dei numeri in posizione dispari è: " + sum);
+	}
+
+}
