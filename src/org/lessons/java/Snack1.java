800f314877fb1190ffd6cd4a6a845e6559ca146e
TALEX1995 <tanase.alex@libero.it>
2023-11-10 11:29:16 +0100

    snack1

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,9 +1,26 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+//		Ask number to user
+		System.out.println("Inserisci un numero");
+		String number = in.nextLine();
+		int intNumber = Integer.parseInt(number);
+		
+		in.close();
+		
+		if(intNumber % 2 == 0) {
+			System.out.println("Il numero inserito è pari: " + intNumber);
+		} else {
+			System.out.println("Il numero inserito è dispari, il numero successivo è: " + (intNumber + 1));
+		}
+		
 
 	}
 
